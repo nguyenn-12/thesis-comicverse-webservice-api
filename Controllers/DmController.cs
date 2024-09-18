02115c7b8db9a14e868edc83056cb76084c6cb27
Mạnh Phạm Đức <21522330@ms.uit.edu.vn>
2024-09-18 08:00:03 +0700

    DmTest

---
+++ Controllers/DmController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using thesis_comicverse_webservice_api.Models;
+
+namespace thesis_comicverse_webservice_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class DmController : ControllerBase
+    {
+        [HttpGet(Name = "DmTest")]
+        public IActionResult Get()
+        {
+            TestDm DucManh = new TestDm
+            {
+                Name = "John Doe",
+                Age = 25
+            };
+            return Ok(DucManh);
+        }
+    }
+}
---
+++ Models/TestDm.cs
@@ -0,0 +1,8 @@
+﻿namespace thesis_comicverse_webservice_api.Models
+{
+    public class TestDm
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
